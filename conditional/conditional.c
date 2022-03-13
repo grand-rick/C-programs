e416132c97960bdc99114f94ad589597e21a864c
Patrick Kabuga <kabugap02@gmail.com>
2022-03-13 14:27:43 +0300

    Corrected the conditional program

--- conditional/conditional.c
+++ conditional/conditional.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-int a, b, x;
+int a, b, x, great;
 
-    int main () {/*Max of three numbers using conditional operators*/
-    printf("Enter three numbers: ");
-    scanf("%d %d %d", &a , &b, &x);
+int main () {
+    /*Max of three numbers using conditional operators*/
+printf("Enter three numbers: ");
+scanf("%d %d %d", &a , &b, &x);
 
-    a>b?(a>x?printf("\t%d", a):printf("\t%d", x)):(b>x?printf("\t%d", b):printf("\t%d", x));
+//a>b?(a>x?printf("\t%d", a):printf("\t%d", x)):(b>x?printf("\t%d", b):printf("\t%d", x));
+    great = (a>b&&a>x)?a:(b>a&&b>x)?b:x;
+    printf("The greatest number is: %d", great);
 
-        return 0;
-    }
+    return 0;
+}
