24f8e449d57c11c3e3f30587b669c93e1267125e
Patrick Kabuga <kabugap02@gmail.com>
2022-03-18 01:11:19 +0300

    Used switch construct to solve the deck of cards problem

--- card_decks/card_decks.c
+++ card_decks/card_decks.c
@@ -12,7 +12,7 @@ int main(void)
      printf("Enter the card rank : ");
      scanf("%c", &rank);
 
-     if (rank=='0') {
+     /*if (rank=='0') {
         points = 10;
      } else if (rank=='1') {
         points = 9;
@@ -34,6 +34,40 @@ int main(void)
         points = 1;
      } else {
         points = 11;
+     }*/
+     switch (rank) {
+        case '0' :
+            points = 10;
+            break;
+        case '1' :
+            points = 9;
+            break;
+        case '2' :
+            points = 8;
+            break;
+        case '3' :
+            points = 7;
+            break;
+        case '4' :
+            points = 6;
+            break;
+        case '5' :
+            points = 5;
+            break;
+        case '6' :
+            points = 4;
+            break;
+        case '7' :
+            points = 3;
+            break;
+        case '8' :
+            points = 2;
+            break;
+        case '9' :
+            points = 1;
+            break;
+        default :
+            points = 11;
      }
 
      printf("The value of the card is %d points\n", points);
