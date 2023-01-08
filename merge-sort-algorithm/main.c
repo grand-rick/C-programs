11f75235de3609fb47d18a4eaa0ac66df29de060
Patrick Kabuga <kabugap02@gmail.com>
2023-01-08 04:39:23 +0300

    Refreshed on C

--- merge-sort-algorithm/main.c
+++ merge-sort-algorithm/main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+int main(void) {
+    int arrayLength = 5;
+    int data[arrayLength];
 
-int main (void)
-{
+    printf("Enter 5 digits ");
+    for (int i = 0; i < arrayLength; i++)
+    {
+        scanf("%d", &data[i]);
+    }
+
+    printf("The data you have entered is: ");
+    for (int i = 0; i < arrayLength; i++) {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
     return 0;
 }
